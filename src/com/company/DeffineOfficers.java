6faac7d90d7166d42f64afe18fd76d1c600ddc8f
IneseKo <in.kocina@gmail.com>
2021-10-25 00:43:38 +0300

    Assignment 1 unfinished but added Define Officers method

---
+++ src/com/company/DeffineOfficers.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class DeffineOfficers {
+    public static void main(String[] args) {
+        Officer officer1 = new Officer();
+        Officer officer2 = new Officer();
+        Officer officer3 = new Officer();
+        System.out.println(officer1);
+        System.out.println(officer2);
+        System.out.println(officer3);
+Officer[] district99 = {officer1, officer2, officer3};
+howManyLevel1(district99);
+    }
+    public static void howManyLevel1(Officer[] district99){
+        for(offic : district99){
+            if ()
+        }
+    }
+}
